f3f54a587787d87c1befe16a0f8a7f7dcb75049d
nthhanh <hanhnguyen.110902@gmail.com>
2024-10-06 22:42:32 +0700

    Building Browse Page (Browse)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
 import Login from "./components/auth/Login";
 import Jobs from './components/Jobs';
+import Browse from './components/Browse';
 
 const appRouter = createBrowserRouter([
 	{
@@ -23,6 +24,10 @@ const appRouter = createBrowserRouter([
 		path: "/jobs",
 		element: <Jobs />
 	},
+	{
+		path: "/browse",
+		element: <Browse />
+	  },
 ]);
 
 function App() {
