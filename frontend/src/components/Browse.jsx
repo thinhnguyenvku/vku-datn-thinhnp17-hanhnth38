6025d8dfee92a5bf7ef5ed288c44b3c5c4454bed
thinhnguyenvku <fuocthinh411@gmail.com>
2024-10-07 22:09:57 +0700

    DB BE - Browse page

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,19 +1,23 @@
-import Job from "./Job";
 import NavBar from "./shared/NavBar";
+import Job from "./Job";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useSelector } from "react-redux";
 
-const randomJobs = [1, 2, 3];
+// const randomJobs = [1, 2,45];
 
 const Browse = () => {
+	useGetAllJobs();
+	const { allJobs } = useSelector((store) => store.job);
 	return (
 		<div>
 			<NavBar />
 			<div className="max-w-7xl mx-auto my-10">
 				<h1 className="font-bold text-xl my-10">
-					Search Results ({randomJobs.length})
+					Search Results ({allJobs.length})
 				</h1>
 				<div className="grid grid-cols-3 gap-4">
-					{randomJobs.map((item, index) => {
-						return <Job />;
+					{allJobs.map((job) => {
+						return <Job key={job._id} job={job} />;
 					})}
 				</div>
 			</div>
