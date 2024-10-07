1ac623d82902d6f1daba14c7dfaff9095d410180
thinhnguyenvku <fuocthinh411@gmail.com>
2024-10-07 21:54:57 +0700

    using redux-persist to fix bug login session

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,22 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Toaster } from "./components/ui/sonner.jsx";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import { StrictMode } from "react";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Toaster } from './components/ui/sonner.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-		<Toaster />
-	</StrictMode>
-);
+const persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+)
